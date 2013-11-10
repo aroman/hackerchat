481526e2e4c1e906d069d18b93280a99a047d8b6
Victor Lourng <hello@victorlourng.com>
2013-11-10 09:12:55 -0500

    mobile layout and js fix

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -6,28 +6,14 @@ if (path.indexOf("/app") != -1) {
 var link = $("a[href='" + path + "']");
 link.parent().addClass('active');
 
-$(window).resize(function() {
-    $('#chatbox').height(
-        $(window).height() - $('window').height() - 330
-    );
-
-});
-
 $(document).ready(function() {
     $('#chatbox').height(
-        $(.nameprompt).height() - $('window').height() - 330
+        $(window).height() - $('window').height() - 350
     );
 });
 
 $(window).resize(function() {
-    $('.sendbox').width(
-        $(window).width() - $('.nameprompt').width() - 65
-    );
-});
-
-
-$(document).ready(function() {
-    $('.sendbox').width(
-        $(.nameprompt).width() - $('.nameprompt').width() - 65
+    $('#chatbox').height(
+        $(window).height() - $('window').height() - 350
     );
 });
